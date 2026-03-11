daeb5e9375a56af81c7a3d8234f2d3e4ba4cc097
Noah! <noahdsouza03@gmail.com>
2026-03-10 22:54:07 -0400

    Import hands and drawing utilities from MediaPipe

--- hand_test.py
+++ hand_test.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+from mediapipe.python.solutions import hands as mp_hands
+from mediapipe.python.solutions import drawing_utils as mp_drawing
 
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
